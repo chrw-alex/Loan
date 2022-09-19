ecb2dca77e226a2b8bf8661e79b2e2bd865a5420
Aleksandra Chervonets <chervonets.as@gmail.com>
2022-09-19 16:18:40 +0300

    add videoplayer

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ window.addEventListener('DOMContentLoaded', () => {
         } else {
           this.hanson.classList.remove('slideInUp');
         }
-      } catch(e) {}
+      } catch (e) { }
 
 
 
@@ -55,7 +55,7 @@ window.addEventListener('DOMContentLoaded', () => {
     render() {
       try {
         this.hanson = document.querySelector('.hanson');
-      } catch(e){}
+      } catch (e) { }
 
       this.btns.forEach(item => {
         item.addEventListener('click', () => {
@@ -75,6 +75,63 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const slider = new Slider('.page', '.next');
   slider.render();
+
+  //playVideo
+
+  class VideoPlayer {
+    constructor(triggers, overlay) {
+      this.btns = document.querySelectorAll(triggers);
+      this.overlay = document.querySelector(overlay);
+      this.close = this.overlay.querySelector('.close');
+    }
+
+    bindTriggers() {
+      this.btns.forEach(btn => {
+        btn.addEventListener('click', () => {
+
+          if (document.querySelector('iframe#frame')) {
+            this.overlay.style.display = 'flex';
+          } else {
+            const path = btn.getAttribute('data-url');
+
+            this.createPlayer(path);
+          }
+
+        });
+      });
+    }
+
+    bindCloseBtn() {
+      this.close.addEventListener('click', () => {
+        this.overlay.style.display = 'none';
+        this.player.stopVideo();
+      });
+    }
+
+    createPlayer(url) {
+      this.player = new YT.Player('frame', {
+        height: '100%',
+        width: '100%',
+        videoId: `${url}`
+      });
+
+      this.overlay.style.display = 'flex';
+    }
+
+    init() {
+      const tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      const firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      this.bindTriggers();
+      this.bindCloseBtn();
+    }
+  }
+
+  const player = new VideoPlayer('.showup .play', '.overlay');
+  player.init();
 
 
 });
